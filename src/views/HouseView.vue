f7840e1295585a03f10a012a38b0e65d049c4e56
DavidKal29 <davidkalmuk65@gmail.com>
2025-10-23 12:52:50 +0200

    Modularizamos la tarjeta que muestra los datos de la casa al querer comprarla

--- src/views/HouseView.vue
+++ src/views/HouseView.vue
@@ -29,19 +29,7 @@
         </form>
 
         <!-- Contenido de información -->
-        <div class="bg-white flex flex-col xl:flex-row max-[360px]:w-[18rem] w-[21rem] h-[30rem] sm:h-[35rem] lg:w-[30rem] lg:h-[35rem] xl:w-[60rem] xl:h-[30rem] rounded-[10px]">
-            <!-- Imagen de la casa -->
-            <img :src="house?.image" alt="" class="max-[360px]:w-[18rem] w-[21rem] h-[14rem] lg:w-[30rem] xl:w-[20rem] lg:h-[15rem] xl:h-[30rem] xl:w-[35rem] rounded-t-[10px] xl:rounded-l-[10px]">
-            
-            <!-- Datos de la casa -->
-            <div class="flex flex-col items-start p-4 gap-4 lg:gap-4 xl:gap-8">
-                <span class="font-bold text-[20px] lg:text-[35px]">{{ house?.address }}</span>
-                <p class="text-lg">N de habitaciones: {{ house?.bedrooms }}</p>
-                <p class="text-lg">N de baños: {{ house?.bathrooms }}</p>
-                <p class="text-lg">Área: {{ house?.area_m2 }} m2</p>
-                <p class="text-lg">Precio: {{ house?.price }} €</p>
-            </div>
-        </div>
+        <BuyHouseComponent :house="house"></BuyHouseComponent>
         
     </div>
 
@@ -56,11 +44,13 @@
 
 import CircleComponent from '@/components/CircleComponent.vue';
 import HomeLink from '@/components/links/HomeLink.vue';
+import BuyHouseComponent from '@/components/BuyHouseComponent.vue';
 import { toast } from 'vue-sonner';
 export default {
     components:{
         CircleComponent,
-        HomeLink
+        HomeLink,
+        BuyHouseComponent
     },
     data(){
         return {
